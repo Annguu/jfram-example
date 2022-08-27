09419c6ff36dab5ae0487b0a011b4883daf72197
annguu <angu8000@gmail.com>
2022-08-27 14:36:29 -0500

    jframe initial commit

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    String name; // string is denoted by ""
+    int numLimbs;
+
+    public Person (String name, int numLimbs){
+        this.name = name;
+        this.numLimbs = numLimbs;
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+
+    public static void main (String [] args){
+        Person angu = new Person("angu", 4);
+    }\
+}
